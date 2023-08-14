5841e01b8312b5c977c826796951809ba6dee1dc
prazwal1436 <prazwal.parajuli2000@gmail.com>
2023-08-14 14:37:22 +0545

    Bottom Nav success

--- lib/main.dart
+++ lib/main.dart
@@ -48,8 +48,6 @@ List<String> phonenum = [
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     DialerPage(),
     LogsPage(),
